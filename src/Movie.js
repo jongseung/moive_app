66d5de1d8a30f4e74efca7c68893e46ba79eaa48
jongseung <park930902@gmail.com>
2018-11-20 03:53:42 +0900

    작업 완료~

---
+++ src/Movie.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
+import './Movie.css';
+
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div className = "Movie">
+            <div className = "Movie__Column">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className = "Movie__Column">
+                <h1>{title}</h1>
+                <div className ="Movie__Genres">
+                    {genres.map((genre, index) => <MoiveGenre genre={genre} key={index} />)}
+                </div>
+                <div className = "Movie__Synopsis">
+                <LinesEllipsis
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                        />   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function MoviePoster({poster,alt}){
+    return (
+        <img src={poster} alt={alt} title = {alt} className = "Movie__Poster"/> //이미지 위에 마우스 올리면 title 뜨게끔 함.
+    )
+} 
+
+function MoiveGenre({genre}){
+    return (
+        <span className = "Movie__Genre">{genre}    </span>
+    )
+}
+
+Movie.propTypes = {
+    title : PropTypes.string.isRequired,
+    poster :PropTypes.string.isRequired,
+    genres :PropTypes.array.isRequired,
+    synopsis :PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster : PropTypes.string.isRequired,
+    alt : PropTypes.string.isRequired
+}
+
+
+MoiveGenre.propTypes = {
+    genre : PropTypes.string.isRequired
+}
+
+export default Movie
